afa1a6ac033d76fb4d3b2749bfd02fa41aad2952
Asane <yaradayo317@gmail.com>
2022-01-31 15:07:12 -0500

    changed url

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,7 +2,11 @@ const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
   const url =
-    "http://api.weatherstack.com/current?access_key=f15a4558d90149681e1e2f40a1724a64&query=' + latitude + ',' + longtitude + '&units=f";
+    "http://api.weatherstack.com/current?access_key=f15a4558d90149681e1e2f40a1724a64&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -2,7 +2,9 @@ const request = require("request");
 
 const geocode = (address, callback) => {
   const url =
-    "https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json.json?access_token=pk.eyJ1IjoiYXNhbmUxMDgiLCJhIjoiY2t2bHJqcnhnMDN2cTJubW80OGY2bnpscSJ9.-0hyri-XUA-v3JRfGwZXMA&limit=1";
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoiYXNhbmUxMDgiLCJhIjoiY2t2bHJqcnhnMDN2cTJubW80OGY2bnpscSJ9.-0hyri-XUA-v3JRfGwZXMA&limit=1";
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -11,8 +13,8 @@ const geocode = (address, callback) => {
       callback("Unable to find location. Try another search.", undefined);
     } else {
       callback(undefined, {
-        latitude: body.features[0].center[0],
-        longitude: body.features[0].center[1],
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
         location: body.features[0].place_name,
       });
     }
