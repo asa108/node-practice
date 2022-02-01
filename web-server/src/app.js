725dc69320929ad8a7d3667e662b215432e471b1
Asane <yaradayo317@gmail.com>
2022-02-01 17:06:29 -0500

    created 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -38,27 +38,24 @@ app.get("/help", (req, res) => {
   });
 });
 
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Asane",
-      age: 23,
-    },
-    {
-      name: "Adom",
-      age: 20,
-    },
-  ]);
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>about page</h1>");
-});
-
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "It is snowing",
     location: "Boston",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    errorMessage: "Page Not Found",
   });
 });
 
