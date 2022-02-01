456cd8a3ab3d30a21d576b3dc514919c46e16d8c
Asane <yaradayo317@gmail.com>
2022-02-01 13:42:19 -0500

    costomezied views file and left comment

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,10 @@ const express = require("express");
 
 const app = express();
 const puclicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
 app.use(express.static(path.join(puclicPath)));
 
 app.get("", (req, res) => {
