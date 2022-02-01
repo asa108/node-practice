f54e4d632c96d38351976317899e6355df83d43c
Asane <yaradayo317@gmail.com>
2022-02-01 16:49:12 -0500

    installed hbs to use template all pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,20 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
-const puclicPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
 
+// Define paths for express config
+const puclicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// setup handlebars engine and view lcoation
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
 app.use(express.static(path.join(puclicPath)));
 
 app.get("", (req, res) => {
@@ -18,14 +26,15 @@ app.get("", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", {
-    title: "about",
+    title: "About",
+    name: "Asane",
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "Help Page",
-    author: "Asane",
+    title: "Help",
+    name: "Asane",
   });
 });
 
