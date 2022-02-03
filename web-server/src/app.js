7eecfb078b230bf76c65294ef73fbcfe62863807
Asane <yaradayo317@gmail.com>
2022-02-02 20:18:46 -0500

    built JSON http end point

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
@@ -44,11 +46,30 @@ app.get("/weather", (req, res) => {
       error: "Address must need to be set",
     });
   }
-  res.send({
-    forecast: "It is snowing",
-    location: "Boston",
-    address: req.query.address,
+
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
   });
+
+  // res.send({
+  //   forecast: req.query.forecast,
+  //   location: req.query.location,
+  //   address: req.query.address,
+  // });
 });
 
 app.get("/products", (req, res) => {
